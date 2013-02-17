335fb06b055f5bd72862ac43d9312c33de1a85d9
Clement Lee <clementlee0@gmail.com>
2013-02-17 12:53:53 -0700

    Started on Agent class

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+import random
+import multiprocessing
+
+class Agent:
+    """The 'clone' of the simulation; each individual agent/actor keeps
+    its own state and evaluating functions."""
+
+    id = 0
+    life = 100
+    prev_enc = {}
+    def __init__(self):
+        pass
+
 def main():
     print "hi"
 
