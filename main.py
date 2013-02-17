d1ca2e9eda54bd38f8db945987e1511ace4c5d76
Clement Lee <clementlee0@gmail.com>
2013-02-17 13:16:10 -0700

    Started on Prisoner's Dilemma & Cooperative Games

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 import random
 import multiprocessing
 
+class PvPGame:
+    """the equivalent of the Prisoner's Dilemma, a player vs player game in
+    in which sides decide to either cooperate or to defect."""
+    pass
+
+class CoopGame:
+    """A cooperative game in which the more people participating the more
+    benefit there is to gain."""
+    coop = 1.05
+    defect = 1.03
+    def __init__(self, coop = 1.05, defect = 1.03):
+        self.coop = coop
+        self.defect = defect
+    pass
+
 class Agent:
     """The 'clone' of the simulation; each individual agent/actor keeps
     its own state and evaluating functions."""
@@ -9,6 +24,10 @@ class Agent:
     life = 100
     prev_enc = {}
     def __init__(self):
+        pass
+
+    def update(self):
+        """Updates the agent for every single iteration"""
         pass
 
 def main():
