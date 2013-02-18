77662ab6fde8a4efc5f49fb3b628e483f93d2a7d
Clement Lee <clementlee0@gmail.com>
2013-02-18 15:58:01 -0700

    Work on Agent updating

--- main.py
+++ main.py
@@ -72,17 +72,34 @@ class Agent:
     posx = 0 #position along "game board"
     posy = 0
     prev_enc = {} #memory of previous encounters with others, keyed by Agent id
-    def __init__(self, life = 100):
+                  #stores float value
+
+    pos_attr = 0.5 #attraction towards positive outcomes
+    neg_attr = -0.5#attraction towards negative outcomes
+    def __init__(self, life = 100, posx = 0, posy = 0, pos_attr = 0.5,
+                 neg_attr = 0.5):
         self.id = Agent.counter
+        self.life = life
+        self.posx = posx
+        self.posy = posy
+        self.pos_attr = pos_attr
+        self.neg_attr = neg_attr
         Agent.counter += 1
         Agent.map[self.id] = self; #maps id to self, allowing future lookup
-        self.life = life
 
     def update(self):
         """Updates the agent for every single iteration"""
         if self.life <= 0: #death
             del Agent.map[self.id] #removing from lookup is equivalent to death
-        self.life -= 1
+        self.life -= 1 #update lifetime
+
+        #breeding
+        scale = 0.1   #overall breeding rate
+        maxlife = 100.0 #maximum life possible
+        ratio = 2*(maxlife - self.life)/maxlife
+        breedposs = scale * normpdf(ratio)
+        print "Breed chance for agent #"+str(self.id)+", age "+str(self.life)+\
+              " is "+str(breedposs)
 
     def evalpvp(self, pvpgame):
         pass
@@ -104,6 +121,9 @@ def main(): #main method
     print "hi"
     numcpu = multiprocessing.cpu_count()
     pool = multiprocessing.Pool(numcpu)
+    a = Agent()
+    for i in xrange(100):
+        a.update()
 
 if __name__ == '__main__':
     main()
